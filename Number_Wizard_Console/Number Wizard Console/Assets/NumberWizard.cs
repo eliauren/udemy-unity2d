2daf158bfb90c8fb66b7ee4cba10376fa8579076
Aurelien Cerveaux <a.cerveaux@betclicgroup.com>
2021-03-25 12:02:05 +0100

    Unity2D migration repository

---
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Bonjour in there");
+        Debug.Log("Please pick a number, don't tell what it is ...");
+        Debug.Log("Higher number is : " + max);
+        Debug.Log("Lowest number you can pick is : " + min);
+        Debug.Log("Tell me your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {            
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("You hit Enter");
+            StartGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than  ... " + guess);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# udemy-unity2d
+Unity 2D Projects from Udemy courses
